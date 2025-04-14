21d98e03dac53176218362c864c6f9556ead436b
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-14 19:47:46 +0530

    new changes added

--- src/main/java/com/shreya/jspservlet/repository/CustomerRepository.java
+++ src/main/java/com/shreya/jspservlet/repository/CustomerRepository.java
@@ -1,4 +1,15 @@
 package com.shreya.jspservlet.repository;
 
+import com.shreya.jspservlet.model.Customer;
+
+import java.sql.Connection;
+
 public class CustomerRepository {
+
+    public boolean insertCustomer(Customer customer){
+        String query = "Insert into customer (id , name , city , mobileNo , age ) values (? , ? , ? , ? , ?)";
+
+        //try (Connection connection = new C)
+    }
+
 }
--- src/main/java/com/shreya/jspservlet/service/RestaurantService.java
+++ src/main/java/com/shreya/jspservlet/service/RestaurantService.java
@@ -1,4 +1,5 @@
 package com.shreya.jspservlet.service;
 
 public class RestaurantService {
+
 }
